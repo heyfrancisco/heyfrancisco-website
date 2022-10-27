7bcb08f5b6dd0cac1e666a3156c772f0db9bea90
Francisco do O <francisco.ramosdoo@outlook.com>
2022-10-27 14:29:08 +0100

    Updated Metadata for SEO. Added settings file in public folder

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import Card from "../components/UI/Card";
 import About from "../components/Text/About";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="bg-zinc-900	">
-      <Card>
-        <About />
-      </Card>
-    </div>
+    <>
+      <Head>
+        <title>Francisco Ramos do Ó</title>
+        <meta
+          name="description"
+          content="Francisco Ramos do Ó Personal Website"
+        />
+        <meta
+          property="og:title"
+          content="Francisco Ramos do Ó Personal Website"
+        />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <div className="bg-zinc-900">
+        <Card>
+          <About />
+        </Card>
+      </div>
+    </>
   );
 }
