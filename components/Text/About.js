c349e28321516559ed165114f65d4c69de8dbfc6
Francisco do O <francisco.ramosdoo@outlook.com>
2022-10-27 14:05:51 +0100

    Fixed bug on paragraph one again again - alterei todos os apos

--- components/Text/About.js
+++ components/Text/About.js
@@ -2,11 +2,11 @@ const About = () => {
   return (
     <div className="text-white max-w-md m-10">
       <p key={1} className="p-2">
-        Hi 👋🏽 I'm Francisco. I was born and live in Lisbon, Portugal.
+        Hi 👋🏽 I&apos;m Francisco. I was born and live in Lisbon, Portugal.
       </p>
       <p key={2} className="p-2">
         I like learning about technology and make people&apos;s life better with
-        it. I'm currently working @ IBM.
+        it. I&apos;m currently working @ IBM.
       </p>
       <p key={3} className="p-2">
         Outside of my academic ventures, I enjoy traveling and playing sports,
@@ -28,21 +28,21 @@ const About = () => {
         >
           code
         </a>
-        , see my&nbsp;
+        , see my{" "}
         <a
           className="underline underline-offset-4"
           href="https://instragram.com/francisco.doo"
         >
           photos
-        </a>
-        &nbsp; or&nbsp;
+        </a>{" "}
+        or{" "}
         <a
           className="underline underline-offset-4"
           href="https://www.linkedin.com/in/franciscoramosdoo/"
         >
           connect
-        </a>
-        &nbsp;with me.
+        </a>{" "}
+        with me.
       </p>
     </div>
   );
