8593926031c11c997589ad4f5f5791f9e5f529e5
Francisco do O <francisco.ramosdoo@outlook.com>
2022-10-27 13:34:51 +0100

    First website draft. Btw first time using Next.js

---
+++ components/Text/About.js
@@ -0,0 +1,51 @@
+const About = () => {
+  return (
+    <div className="text-white max-w-md m-10">
+      <p key={1} className="p-2">
+        Hi 👋🏽 I'm Francisco. I was born and live in Lisbon, Portugal.
+      </p>
+      <p key={2} className="p-2">
+        I like learning about technology and make people’s life better with it.
+        I'm currently working @ IBM.
+      </p>
+      <p key={3} className="p-2">
+        Outside of my academic ventures, I enjoy traveling and playing sports,
+        mainly rugby. I spend most of my spare time reading about tech, business
+        and finance.
+      </p>
+      <p key={4} className="p-2">
+        Feel free to say{" "}
+        <a
+          className="underline underline-offset-4"
+          href="mailto:talk@heyfrancisco.com"
+        >
+          hello
+        </a>
+        , fork my{" "}
+        <a
+          className="underline underline-offset-4"
+          href="https://github.com/heyfrancisco"
+        >
+          code
+        </a>
+        , see my&nbsp;
+        <a
+          className="underline underline-offset-4"
+          href="https://instragram.com/francisco.doo"
+        >
+          photos
+        </a>
+        &nbsp; or&nbsp;
+        <a
+          className="underline underline-offset-4"
+          href="https://www.linkedin.com/in/franciscoramosdoo/"
+        >
+          connect
+        </a>
+        &nbsp;with me.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Card from "../components/UI/Card";
+import About from "../components/Text/About";
+
+export default function Home() {
+  return (
+    <div className="bg-zinc-900	">
+      <Card>
+        <About />
+      </Card>
+    </div>
+  );
+}
