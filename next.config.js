de42cd09f4d9a62d85ce488bf23c1246dde1af25
Francisco do O <francisco.ramosdoo@outlook.com>
2024-01-17 11:30:56 +0000

    Dockerfile

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: "standalone",
+};
